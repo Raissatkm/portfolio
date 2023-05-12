b8eb04ea8e19190dbf73be6616992b588f4c3d66
AllanSantanna <allanxlink@hotmail.com>
2023-05-12 10:37:00 -0300

    button design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,8 @@ import Text from "@/components/atoms/Text"
 
 import { GlobalStyles } from '@/global';
 import { selectTheme } from '@/global/theme'
+import Button from '@/components/molecules/button';
+import Icon from '@/components/atoms/icon';
 
 export default function Home() {
   const theme = selectTheme({
@@ -11,7 +13,7 @@ export default function Home() {
     font: 'default',
   });
   return (
-    <ThemeProvider theme={theme}>
+    <><ThemeProvider theme={theme}>
       <Text type="H1" color='ACCENTED'>Headline</Text>
       <Text type="H2" color='ACCENTED_DARK'>Headline</Text>
       <Text type="H3">Headline</Text>
@@ -24,6 +26,7 @@ export default function Home() {
       <Text type="Body 2" color='ACCENTED_DARK'>Body 2</Text>
       <Text type="BUTTON" color='ACCENTED'>BUTTON</Text>
       <GlobalStyles />
-    </ThemeProvider>
+      <Button size='large' ><Icon name={'github'} size={'large'} color={'white'} /></Button>
+    </ThemeProvider></>
   )
 }
